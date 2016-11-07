b22a9b290c49b05cea2fd366fdfb2030c05e6a0d
Anton Dort-Golts <dortgolts@gmail.com>
2016-11-07 20:47:53 +0300

    Fix: setup

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     description = 'Alexa Flask Guide for Flask-ASK',
     author = 'Anton Dort-Golts',
     author_email = 'dortgolts@gmail.com',
-    url = 'https://github.com/peterldowns/mypackage',
-    download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+    url = 'https://github.com/dgtony/afg',
+    download_url = 'https://github.com/dgtony/afg/tarball/0.1',
     keywords = ['alexa', 'flask-ask', 'fsm'],
     install_requires=[
         'flask-ask',
